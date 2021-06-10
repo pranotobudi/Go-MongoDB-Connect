501aaac4da026942e102d64ec31ac0d05992cd1f
Pranoto Budi <pranotobudi.s@gmail.com>
2021-06-10 10:31:57 +0700

    update

--- mongo_local/tasker/main.go
+++ mongo_local/tasker/main.go
@@ -27,6 +27,7 @@ var collection *mongo.Collection
 var ctx = context.TODO()
 
 func init() {
+
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
